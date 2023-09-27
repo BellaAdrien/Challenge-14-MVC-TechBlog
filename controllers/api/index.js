ef06da627ce76f8f2e6777ae73159265c51aac72
BellaAdrien <Iadrien2020@fau.edu>
2023-09-27 14:51:41 -0400

    save14

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
-
+// const projectRoutes = require('./projectRoutes');
+// const homeRoutes = require('./homeRoutes'); 
+const apiRoutes = require('./apiRoutes');
 
 //http://localhost:3001/api/users
 router.use('/users', userRoutes);
 
 //http://localhost:3001/api/projects
-router.use('/projects', projectRoutes);
+// router.use('/projects', projectRoutes);
+
+// router.use('./homeRoutes', homeRoutes);
+
+router.use('./apiRoutes', apiRoutes);
 
 module.exports = router;
